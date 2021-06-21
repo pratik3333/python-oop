e9bd9107e06095111c0d22cafa3bbcffdf5ab265
pratik kagale <pratikkagale3333@gmail.com>
2021-06-21 10:22:19 -0700

    array

---
+++ Telusko/2.py
@@ -0,0 +1,5 @@
+from numpy import *
+
+arr=linspace(0,15)
+
+print(arr)
--- Telusko/ARRAY/2.py
+++ Telusko/ARRAY/2.py
@@ -5,3 +5,9 @@ vals=array('i',[2,3,4,5])
 
 for i in range(4):
     print(vals[i])
+#
+# for i in vals:
+#     print(i)
+#
+# for i in range(len(vals)):
+#     print(vals[i])
