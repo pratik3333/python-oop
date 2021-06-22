50468b19d582c214a2009f2b743611c20abf094f
pratik kagale <pratikkagale3333@gmail.com>
2021-06-22 09:48:45 -0700

    Reduce Function (filter,map,reduce)

--- Telusko/FMR/Reduc_e.py
+++ Telusko/FMR/Reduc_e.py
@@ -1,14 +1,11 @@
 from functools import reduce
 
-def add_all(a,b):
-    return a+b
-
 nums=[2,3,4]
 
 evens=list(filter(lambda n:n%2==0,nums))
 double=list(map(lambda n:n*2,evens))
 
-sum=reduce(add_all,double)
+sum=reduce(lambda a,b:a+b,double)
 
 print(evens)
 print(double)
