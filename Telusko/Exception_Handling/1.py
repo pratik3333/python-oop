6adb394b5580886e4d8df068e78308ee1a90e154
pratik kagale <pratikkagale3333@gmail.com>
2021-06-24 11:46:07 -0700

    try,except as Exception as e,finally

--- Telusko/Exception_Handling/1.py
+++ Telusko/Exception_Handling/1.py
@@ -4,10 +4,12 @@ a=4
 b=0
 
 try:
+    print("Resource open")
     print(a/b)
 
 except Exception as e:
     print("hey, you cannot devide a number by zero.",e)
 
-print("bye")
+finally:
+    print("Resource close")
 
