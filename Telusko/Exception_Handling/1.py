a46514c14a6b88a91c86119770915f17e0abb581
pratik kagale <pratikkagale3333@gmail.com>
2021-06-24 12:51:06 -0700

    Exception Handling

--- Telusko/Exception_Handling/1.py
+++ Telusko/Exception_Handling/1.py
@@ -6,10 +6,18 @@ b=0
 try:
     print("Resource open")
     print(a/b)
+    k=int(input("Enter the number":))
+    print(k)
 
-except Exception as e:
+
+except ZeroDivisionError as e:
     print("hey, you cannot devide a number by zero.",e)
 
+except ValueError as e:
+    print("Invalid input")
+
+except Exception as e:
+    print("Something went wrong...")
 finally:
     print("Resource close")
 
